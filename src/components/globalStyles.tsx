bf707b1d347b8862fdfe8a94b819be4d4fe05437
Ryan <ryan.j.forgie@gmail.com>
2019-02-28 19:03:06 +0000

    Remove more template code and add basic grid

--- src/components/globalStyles.tsx
+++ src/components/globalStyles.tsx
@@ -6,6 +6,14 @@ const GlobalStyles = createGlobalStyle`
     --primaryFont: Arial, monospace;
     --sansFont: 'Arial', sans-serif;
 }
+
+html {
+    box-sizing: border-box;
+}
+
+body {
+    background-color: #3b5887;
+}
 `
 
 export default GlobalStyles
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import styled from 'styled-components'
 import GlobalStyles from './GlobalStyles'
 import { StaticQuery, graphql } from "gatsby"
+import "./layout.css"
 
 import Header from "./header"
 
@@ -20,22 +21,14 @@ const Layout = ({ children }) => (
     render={data => (
       <>
       <GlobalStyles />
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+      <div className="grid">
+        <div className="item">hello</div>
+        <div className="item">hello</div>
+        <div className="item">hello</div>
+        <div className="item">hello</div>
+        <div className="item">hello</div>
+        <div className="item">hello</div>
+      </div>
       </>
     )}
   />
