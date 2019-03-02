102c11425b42629c8e788975613c461d28ee8307
Ryan <ryan.j.forgie@gmail.com>
2019-03-02 18:26:03 +0000

    Add grid to index and convert to styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,30 +3,26 @@ import PropTypes from "prop-types"
 import styled from 'styled-components'
 import GlobalStyles from './GlobalStyles'
 import { StaticQuery, graphql } from "gatsby"
-import "./layout.css"
 
 import Header from "./header"
 
 const Container = styled.div`
   display: grid;
   grid-template-columns: 0.5fr 1fr;
-  grid-template-rows: 1fr;
   height: 100vh;
 `
 const ItemWrapper = styled.div`
-  display: grid;
   background-color: #c6d1e2;
   justify-content: center;
   align-items: center;
   border: 5px solid black;
 `
 const Nav = styled(ItemWrapper)`
-  grid-row: span 3;
+  grid-column-start: 1;
 `
 
 const Content = styled(ItemWrapper)`
   grid-column-start: 2;
-  grid-row-start: 1;
 `
 
 const Layout = ({ children }) => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,34 @@ import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import "./index.css"
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 100vh;
+`
+const ItemWrapper = styled.div`
+  background-color: red;
+  justify-content: center;
+  align-items: center;
+  border: 5px solid black;
+`
+const Photo = styled(ItemWrapper)`
+  grid-column-start: 1;
+`
+
+const Card = styled(ItemWrapper)`
+  grid-column-start: 2;
+`
 
 const IndexPage = () => (
   <Layout>
   <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />    
-    <div className='grid'>
-      <div className='item image-wrapper'>photo</div>
-      <div className='item card-wrapper'>card</div>
-    </div>
+    <Container className='grid'>
+      <Photo>photo</Photo>
+      <Card>card</Card>
+    </Container>
   </Layout>
 )
 
